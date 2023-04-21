0d0754aaeb3e78a762f4f63fcfd2689a16962ee4
Ulises MR <umr.user@gmail.com>
2023-04-21 10:40:32 -0600

    v1 Métodos

---
+++ Cuentas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace POOU3C_Ejemplo1
+{
+    class Cuentas
+    {
+        // ¿Qué contiene una clase?
+        // Miembros: Campos, Métodos, Contructores, Propiedades, Destrctores
+
+        //Métodos
+        public int CalcularCosto() {
+            return 10+10;
+        }
+        public double CalcularCosto1( int cantidad, double precio ) {
+            return cantidad * precio;
+        }
+
+        public string CalcularCosto2(string nombreArticulo, int cantidad, double precio) {
+            double total = cantidad * precio;
+            return "Producto: " + nombreArticulo + " Total de venta: $" + total ;
+        }        
+        public void CalcularCosto3(string nombreArticulo, int cantidad, double precio)
+        {
+            double total = cantidad * precio;
+            string resultado = "Producto: " + nombreArticulo + " Total de venta: $" + total;
+            Console.WriteLine(resultado);
+        }
+
+        public void PersonalizarConsola() {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Title = "BancoTec";
+            Console.WindowHeight = 40;
+            Console.WindowWidth = 100;
+            Console.Clear();
+        }
+
+    }
+    /*
+        Participaciones:
+          + Aldo, Sergio, Juan Ángel, MArijose
+          - ALexis, UMR, UMR,
+     */
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace POOU3C_Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instranciar la clase
+            Cuentas cuenta1 = new Cuentas();
+            cuenta1.PersonalizarConsola();
+            // Mandar llamar los mienbros
+            int resultado = cuenta1.CalcularCosto();
+            double resultado1 = cuenta1.CalcularCosto1(10, 14.5);
+            string resultado2 = cuenta1.CalcularCosto2("Goicochea", 20, 200);
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado1 );
+            Console.WriteLine(resultado2);
+            //Mandar llamar el metodo de tipó void
+            cuenta1.CalcularCosto3("Marcador TOP", 3, 20.99);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
